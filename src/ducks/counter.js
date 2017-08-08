f9d6ebec234c1dd49cbd204d6ca633ac6c5e1666
Fernando De la Vega <fernando.delavega2847@gcssk12.net>
2017-08-08 14:13:17 -0600

    Added functionality to the project.

--- src/ducks/counter.js
+++ src/ducks/counter.js
@@ -1,2 +1,37 @@
 // change this to true to see test results on the black diamond section.
 export const BLACK_DIAMOND = false;
+const DECREMENT = 'DECREMENT'
+const INCREMENT = 'INCREMENT'
+
+export function decrement(amnt){
+    return {
+        type: DECREMENT,
+        amnt
+    }
+}
+export function increment(amnt){
+    return {
+        type: INCREMENT,
+        amnt
+    }
+}
+
+
+let initialState = {
+    currentValue: 0
+}
+export default function(state = initialState, action){
+    switch (action.type) {
+        case INCREMENT:
+            return Object.assign(
+                {},
+                state,
+                {currentValue: state.currentValue + action.amnt})
+        case DECREMENT:
+            return Object.assign(
+                {},
+                state,
+                {currentValue: state.currentValue - action.amnt})
+    }
+    return state;
+}
